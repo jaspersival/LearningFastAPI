88979dc80cc2507504ce50bee96071df28078154
Jasper Sival <jaspersival@gmail.com>
2021-11-29 10:02:44 +0100

    Implement get model

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 @app.get("/")
@@ -8,6 +15,15 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/items/{item_id}")
+async def say_hello(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
