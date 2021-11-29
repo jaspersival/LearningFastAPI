0ac88cd4b7a5515082608933f47e0da173448be3
Jasper Sival <jaspersival@gmail.com>
2021-11-29 12:14:30 +0100

    Create item with tax logic

--- main.py
+++ main.py
@@ -22,7 +22,11 @@ class ModelName(str, Enum):
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
 @app.get("/")
