c60e48af6c0a28308a96f2bec15ad3deb4f93fca
Jasper Sival <jaspersival@gmail.com>
2021-11-29 11:34:31 +0100

    Query with bool argument

--- main.py
+++ main.py
@@ -21,7 +21,12 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 
 
 @app.get("/items/{item_id}")
-async def read_items(item_id: str, q: Optional[str] = None):
+async def read_items(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
     if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long " "description"}
+        )
+    return item
