24b93f61372cd46da1111c5d15236b31e1f8d7c7
Jasper Sival <jaspersival@gmail.com>
2021-11-29 11:25:29 +0100

    Implement read_items with fake_items

--- main.py
+++ main.py
@@ -20,15 +20,9 @@ async def say_hello(item_id: int):
     return {"item_id": item_id}
 
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW"}
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    return {"model_name": model_name, "message": "Have some residuals"}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+@app.get("/items")
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
