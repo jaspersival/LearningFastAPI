f1379a439f9cc9d354c362b1623692a856b498ac
Jasper Sival <jaspersival@gmail.com>
2021-11-30 07:40:29 +0100

    Use minimum length and regex for the query in read_items

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ class ModelName(str, Enum):
 
 
 @app.get("/items/")
-async def read_items(q: Optional[str] = Query(None, max_length=50)):
+async def read_items(
+    q: Optional[str] = Query(None, min_length=3, max_length=50, regex="^fixed_query$")
+):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
