70ada3d8edc89c178ad6566c94ec5d39cfc0422c
Jasper Sival <jaspersival@gmail.com>
2021-12-01 10:39:51 +0100

    Use header in HTTPException

--- main.py
+++ main.py
@@ -13,5 +13,7 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: str):
     if item_id not in items:
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(
+            status_code=404, detail="Item not found", headers={"X-Error": "There goes my error"}
+        )
     return {"item": items[item_id]}
