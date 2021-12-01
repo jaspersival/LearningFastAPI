bc4eaa4423a00b6c62a8bd3525be044f68da4693
Jasper Sival <jaspersival@gmail.com>
2021-12-01 11:55:17 +0100

    Description from docstring

--- main.py
+++ main.py
@@ -16,8 +16,21 @@ class Item(BaseModel):
     tags: set[str] = []
 
 
-@app.post("/items/", response_model=Item, tags=["items"])
+@app.post(
+    "/items/",
+    response_model=Item,
+    summary="Create an item",
+)
 async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
     return item
 
 
