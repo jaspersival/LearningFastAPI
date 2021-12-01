0281047f7f0517d133e05db45d2a98da5df806df
Jasper Sival <jaspersival@gmail.com>
2021-12-01 11:51:30 +0100

    Add tags

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ class Item(BaseModel):
     tags: set[str] = []
 
 
-@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED)
+@app.post("/items/", response_model=Item, tags=["items"])
 async def create_item(item: Item):
     return item
+
+
+@app.get("/items/", tags=["items"])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
+
+
+@app.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "johndoe"}]
