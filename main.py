9843c2f10470df2b330b5b787964db9fb713d286
Jasper Sival <jaspersival@gmail.com>
2021-12-01 09:39:41 +0100

    Multiple file uploads

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from fastapi.responses import HTMLResponse
 from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
@@ -12,10 +13,27 @@ class ModelName(str, Enum):
 
 
 @app.post("/files/")
-async def create_file(file: bytes = File(...)):
-    return {"file_size": len(file)}
+async def create_files(files: list[bytes] = File(...)):
+    return {"file_sizes": [len(file) for file in files]}
 
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    return {"file_name": file.filename}
+@app.post("/uploadfiles/")
+async def create_upload_files(files: list[UploadFile] = File(...)):
+    return {"file_names": [file.filename for file in files]}
+
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
