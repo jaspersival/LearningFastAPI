718df4d486e579a76b64df41a4130815709d633a
Jasper Sival <jaspersival@gmail.com>
2021-12-03 07:25:37 +0100

    Use sub dependencies

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from typing import Optional
 
-from fastapi import FastAPI, Depends
-from enum import Enum
+from fastapi import FastAPI, Depends, Cookie
 
 app = FastAPI()
 
 
-async def common_parameters(q: Optional[str] = None, skip: int = 0, limit: int = 100):
-    return {"q": q, "skip": skip, "limit": limit}
+def query_extractor(q: Optional[str] = None):
+    return q
+
+
+def query_or_cookie_extractor(
+    q: str = Depends(query_extractor), last_query: Optional[str] = Cookie(None)
+):
+    if not q:
+        return last_query
+    return q
 
 
 @app.get("/items/")
-async def read_items(commons: dict = Depends(common_parameters)):
-    return commons
-
-
-@app.get("/users/")
-async def read_users(commons: dict = Depends(common_parameters)):
-    return commons
+async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
+    return {"q_or_cookie": query_or_default}
