47bbe3d21b5d2ecb9fc262aeb43ebe192a99feb6
Jasper Sival <jaspersival@gmail.com>
2021-12-03 07:44:15 +0100

    Global dependencies

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from typing import Optional
 
 from fastapi import FastAPI, Depends, Cookie, Header, HTTPException
 
-app = FastAPI()
-
 
 async def verify_token(x_token: str = Header(...)):
     if x_token != "fake-super-secret-token":
@@ -16,6 +14,14 @@ async def verify_key(x_key: str = Header(...)):
     return x_key
 
 
-@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+
+
+@app.get("/items/")
 async def read_items():
-    return [{"item": "Foo"}, {"item": "Bar"}]
+    return [{"item": "Portal Gun"}, {"item": "Plumbus"}]
+
+
+@app.get("/users/")
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
